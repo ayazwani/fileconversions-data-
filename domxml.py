2d398d30b88b341948378e2d1ff8554612a91652
Ayaz Wani <wani.ayazashraf@gmail.com>
2020-10-16 13:25:45 +0530

    Parsing xml using mimi dom module of python

--- domxml.py
+++ domxml.py
@@ -1,9 +1,26 @@
-import xml.dom.minidom as dom
+import xml.dom.minidom
 
-domparser = dom("NCT00571389")
+domtree = xml.dom.minidom.parse("NCT00571389.xml")
 
-group = domtree.documentElement
+root = domtree.documentElement
 
-print(group)
+print(root)	
 
-#ele = group.getElementsByTagName("")
+nct_id = root.getElementsByTagName("nct_id")
+print(nct_id[0].firstChild.data)
+
+condition = root.getElementsByTagName("condition")
+for i,tags in enumerate(condition):
+	print(condition[i].firstChild.data)
+
+name = root.getElementsByTagName("name")
+print(name[0].firstChild.data)
+
+city = root.getElementsByTagName("city")
+print(city[0].firstChild.data)
+
+criteria = root.getElementsByTagName("textblock")
+print(criteria[0].firstChild.data)
+print(criteria[1].firstChild.data)
+print(criteria[2].firstChild.data)
+print(criteria[3].firstChild.data)
